1e9a7932c42682ca140821a3c0029b78b49e485f
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2017-05-19 16:09:48 -0300

    chore(tests): Add node-red helper
    Which is necessary to run node-red node tests.

--- test/unit/helpers.js
+++ test/unit/helpers.js
@@ -1,6 +1,19 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import nock from 'nock';
+import path from 'path';
 
 chai.use(sinonChai);
 
 global.expect = chai.expect;
+process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || path.resolve(`${__dirname}/../../node_modules/node-red`);
+
+// eslint-disable-next-line
+const helper = require(path.join(process.env.NODE_RED_HOME, 'test', 'nodes', 'helper.js'));
+
+try {
+  helper.nock = helper.nock || nock;
+} catch (er) {
+  helper.nock = null;
+}
+module.exports = helper;
